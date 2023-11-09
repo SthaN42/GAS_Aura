8e6f1c4c0eca52dde014f656769e815504562e42
Nathan Guerin <nathan.guerin87@gmail.com>
2023-11-09 18:02:35 +0100

    Feat : CharacterClassInfo DataAsset

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Nathan Guerin
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
