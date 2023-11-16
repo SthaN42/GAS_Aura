7dd6ad1054b6770c77c1a2969003ec592a0a6908
Nathan Guerin <nathan.guerin87@gmail.com>
2023-11-16 17:30:20 +0100

    Feat: Causing melee damage

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -3,3 +3,16 @@
 
 #include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
+{
+	const FGameplayEffectSpecHandle DamageSpecHandle = MakeOutgoingGameplayEffectSpec(DamageEffectClass, 1.f);
+	for (TTuple<FGameplayTag, FScalableFloat> Pair : DamageTypes)
+	{
+		const float ScaledMagnitude = Pair.Value.GetValueAtLevel(GetAbilityLevel());
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(DamageSpecHandle, Pair.Key, ScaledMagnitude);
+	}
+	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(), UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor));
+}
