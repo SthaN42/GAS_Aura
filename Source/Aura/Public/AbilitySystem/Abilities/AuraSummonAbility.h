92d0f788c71a9d180a42713de3d3cc7595f2e21f
Nathan Guerin <nathan.guerin87@gmail.com>
2023-11-20 18:47:29 +0100

    Feat: Spawning minions

--- Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -18,18 +18,18 @@ public:
 	UFUNCTION(BlueprintCallable)
 	TArray<FVector> GetSpawnLocations(bool bDrawDebugShapes);
 
-	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Summoning")
 	int32 NumMinionsToSpawn = 5;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Summoning")
 	TArray<TSubclassOf<APawn>> MinionClasses;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Summoning")
 	float MinSpawnDistance = 50.f;
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Summoning")
 	float MaxSpawnDistance = 250.f;
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Summoning")
 	float SpawnSpread = 90.f;
 };
