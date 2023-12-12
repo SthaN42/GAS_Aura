39b4b50dee199d158ca343d77c5459b9edb73f35
Nathan Guerin <nathan.guerin87@gmail.com>
2023-12-13 00:01:09 +0100

    Feat: Aura beam spell

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -0,0 +1,33 @@
+// Copyright Nathan Guerin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraBeamSpell : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Beam Spell")
+	void StoreMouseDataInfo(const FHitResult& HitResult);
+	
+	UFUNCTION(BlueprintCallable, Category = "Beam Spell")
+	void StoreOwnerPlayerController();
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	FVector MouseHitLocation;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<AActor> MouseHitActor;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+};
