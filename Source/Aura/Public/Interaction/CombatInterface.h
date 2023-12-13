7fc35368bf094851dbf7a4a14391b62869cd02e1
Nathan Guerin <nathan.guerin87@gmail.com>
2023-12-13 16:00:10 +0100

    Feat: GameplayCueNotify actor

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -103,4 +103,7 @@ public:
 
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
 	void SetInShockLoop(bool bInLoop);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	USkeletalMeshComponent* GetWeapon();
 };
