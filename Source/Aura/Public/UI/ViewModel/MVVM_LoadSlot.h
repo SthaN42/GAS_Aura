184cbc331402773f06537993f6905c4456c8569b
Nathan Guerin <nathan.guerin87@gmail.com>
2024-03-17 00:51:54 +0100

    Feat: LoadSlot ViewModel

---
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -0,0 +1,28 @@
+// Copyright Nathan Guerin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "MVVM_LoadSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UMVVM_LoadSlot : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	/* Field Notifies */
+
+	void SetLoadSlotName(const FString& InLoadSlotName);
+
+	FString GetLoadSlotName() const { return LoadSlotName; }
+	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess = "true"))
+	FString LoadSlotName;
+};
