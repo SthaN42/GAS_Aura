717c48b70e6b4a4294c811bde5250efd5fed0970
Nathan Guerin <nathan.guerin87@gmail.com>
2024-03-18 15:37:22 +0100

    Feat: Switching the widget switcher

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -6,6 +6,8 @@
 #include "MVVMViewModelBase.h"
 #include "MVVM_LoadSlot.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSetWidgetSwitcherIndex, int32, WidgetSwitcherIndex);
+
 /**
  * 
  */
@@ -15,6 +17,10 @@ class AURA_API UMVVM_LoadSlot : public UMVVMViewModelBase
 	GENERATED_BODY()
 
 public:
+	void InitializeSlot();
+	
+	UPROPERTY(BlueprintAssignable)
+	FSetWidgetSwitcherIndex SetWidgetSwitcherIndex;
 	
 	/* Field Notifies */
 
